06115ff187d2916869aa8b3fb792eea0a31eb2ce
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2023-09-19 16:17:50 -0300

    feat: added support to comment on answer

---
+++ src/infra/http/controllers/comment-on-answer.controller.e2e-spec.ts
@@ -0,0 +1,71 @@
+import { AppModule } from '@/infra/app.module'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+import { AnswerFactory } from 'test/factories/make-answer'
+import { QuestionFactory } from 'test/factories/make-question'
+import { StudentFactory } from 'test/factories/make-student'
+
+describe('Comment on answer (E2E)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
+  let answerFactory: AnswerFactory
+
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory, AnswerFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    prisma = moduleRef.get<PrismaService>(PrismaService)
+    studentFactory = moduleRef.get<StudentFactory>(StudentFactory)
+    questionFactory = moduleRef.get<QuestionFactory>(QuestionFactory)
+    answerFactory = moduleRef.get<AnswerFactory>(AnswerFactory)
+    jwt = moduleRef.get<JwtService>(JwtService)
+
+    await app.init()
+  })
+
+  test('[POST] /answers/:answerId/comments', async () => {
+    const user = await studentFactory.makePrismaStudent()
+
+    const accessToken = jwt.sign({ sub: user.id.toString() })
+
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+    })
+    const questionId = question.id
+
+    const answer = await answerFactory.makePrismaAnswer({
+      authorId: user.id,
+      questionId,
+    })
+
+    const answerId = answer.id.toString()
+
+    const response = await request(app.getHttpServer())
+      .post(`/answers/${answerId}/comments`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        content: 'New comment on answer',
+      })
+
+    expect(response.statusCode).toBe(201)
+
+    const commentsOnDatabase = await prisma.comment.findFirst({
+      where: {
+        content: 'New comment on answer',
+      },
+    })
+
+    expect(commentsOnDatabase).toBeTruthy()
+  })
+})

906952921f572f43383764186271c93cb7a3a009
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2023-09-19 15:24:35 -0300

    feat: added support to choose best answer on question

--- src/domain/forum/application/use-cases/choose-question-best-answer.ts
+++ src/domain/forum/application/use-cases/choose-question-best-answer.ts
@@ -4,6 +4,7 @@ import { Question } from '../../enterprise/entities/question'
 import { Either, left, right } from '@/core/either'
 import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error'
 import { NotAllowedError } from '@/core/errors/not-allowed-error'
+import { Injectable } from '@nestjs/common'
 
 interface ChooseQuestionBestAnswerUseCaseRequest {
   authorId: string
@@ -16,6 +17,7 @@ type ChooseQuestionBestAnswerUseCaseResponse = Either<
   }
 >
 
+@Injectable()
 export class ChooseQuestionBestAnswerUseCase {
   constructor(
     private answerRepository: AnswersRepository,
